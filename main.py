2c780f50d37615ac4e43bc03153b580300dc9373
Pasquini08 <143645769+Pasquini08@users.noreply.github.com>
2024-03-05 15:58:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+# JOGOS 
 
+Time1 = input ("Coloque o Time que jogou:")
+numero1 = int(input ("Numero de gols"))
+Time2 = input ("Coloque o Time adversario:")
+numero2 = int(input ("Numero de gols adversario"))
+if numero1 > numero 2:
+   print ("
+
+   
